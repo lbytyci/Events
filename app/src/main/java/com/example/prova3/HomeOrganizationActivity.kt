7127d18220a7a85b286eda1acb8691f8ee00718a
Leonite1 <122058216+Leonite1@users.noreply.github.com>
2024-12-28 14:32:42 +0000

    Update HomeOrganizationActivity.kt

--- app/src/main/java/com/example/prova3/HomeOrganizationActivity.kt
+++ app/src/main/java/com/example/prova3/HomeOrganizationActivity.kt
@@ -1,1 +1,16 @@
+package com.example.prova3
 
+import android.app.AlertDialog
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.*
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.prova3.utils.Event
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.database.*
