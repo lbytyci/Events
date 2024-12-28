733c1a2f6224746e2260e127f5f3e1988c0a9a15
Leonite1 <122058216+Leonite1@users.noreply.github.com>
2024-12-28 14:33:31 +0000

    Update HomeOrganizationActivity.kt

--- app/src/main/java/com/example/prova3/HomeOrganizationActivity.kt
+++ app/src/main/java/com/example/prova3/HomeOrganizationActivity.kt
@@ -14,3 +14,25 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.prova3.utils.Event
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.database.*
+
+
+class HomeOrganizationActivity : AppCompatActivity() {
+
+    private lateinit var database: DatabaseReference
+    private lateinit var eventAdapter: EventAdapter
+    private val eventList = mutableListOf<Event>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home_organization)
+
+        supportActionBar?.title = "Home"
+
+
+        try {
+            database = FirebaseDatabase.getInstance().getReference("Events")
+        } catch (e: Exception) {
+            Toast.makeText(this, "Firebase initialization error: ${e.message}", Toast.LENGTH_SHORT)
+                .show()
+        }
