4baf8fdc397ec09d399f014a37deb851fed6a999
Leonite1 <122058216+Leonite1@users.noreply.github.com>
2024-12-28 21:12:21 +0000

    Update HomeOrganizationSearchActivity.kt

--- app/src/main/java/com/example/prova3/HomeOrganizationSearchActivity.kt
+++ app/src/main/java/com/example/prova3/HomeOrganizationSearchActivity.kt
@@ -1,1 +1,64 @@
+package com.example.prova3
 
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.prova3.utils.Event
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+
+class HomeOrganizationSearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home_organization_search)
+
+        supportActionBar?.title = "Search"
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNavigationView.selectedItemId = R.id.search
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> navigateToHome()
+                R.id.search -> true
+                R.id.tickets -> {
+                    startActivity(Intent(applicationContext, TicketsActivity::class.java))
+                    finish()
+                    true
+                }
+                R.id.profile -> {
+                    startActivity(Intent(applicationContext, ProfileActivity::class.java))
+                    finish()
+                    true
+                }
+                else -> false
+            }
+        }
+
+
+        val searchButton: Button = findViewById(R.id.search_button)
+        val searchInput: EditText = findViewById(R.id.search_input)
+        val recyclerView: RecyclerView = findViewById(R.id.mainRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val eventAdapter = EventAdapter(mutableListOf())
+        recyclerView.adapter = eventAdapter
