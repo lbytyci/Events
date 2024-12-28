b615e65b029f96c13b8db446b69409afdd018ee6
Leonite1 <122058216+Leonite1@users.noreply.github.com>
2024-12-28 14:35:06 +0000

    Update HomeOrganizationActivity.kt

--- app/src/main/java/com/example/prova3/HomeOrganizationActivity.kt
+++ app/src/main/java/com/example/prova3/HomeOrganizationActivity.kt
@@ -36,3 +36,54 @@ class HomeOrganizationActivity : AppCompatActivity() {
             Toast.makeText(this, "Firebase initialization error: ${e.message}", Toast.LENGTH_SHORT)
                 .show()
         }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        val addTaskBtn = findViewById<ImageView>(R.id.addTaskBtn)
+        addTaskBtn?.setOnClickListener {
+            showAddEventPopup()
+        } ?: Toast.makeText(this, "Add button not found", Toast.LENGTH_SHORT).show()
+
+
+        setupRecyclerView()
+        fetchEventsFromDatabase()
+
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNavigationView.selectedItemId = R.id.home
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> true
+                R.id.search -> {
+                    startActivity(
+                        Intent(
+                            applicationContext,
+                            HomeOrganizationSearchActivity::class.java
+                        )
+                    )
+                    finish()
+                    true
+                }
+
+                R.id.tickets -> {
+                    startActivity(Intent(applicationContext, TicketsActivity::class.java))
+                    finish()
+                    true
+                }
+
+                R.id.profile -> {
+                    startActivity(Intent(applicationContext, ProfileActivity::class.java))
+                    finish()
+                    true
+                }
+
+                else -> false
+            }
+        }
+    }
